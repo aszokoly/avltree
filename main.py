a21989834a9ec2a1d2a3acb46a15c6afce3bc271
aszokoly <154333598+aszokoly@users.noreply.github.com>
2025-04-02 16:03:52 +0000

    class

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from friendsbalt.acs import OrderedMap, StringDiff
 from fsspec import filesystem
 from datetime import datetime
 
-
+#to test: create an infinite loop at bottom and use input command. print out a menu of things you cna do (current veriosn, previous version press2, save current version. then go into file and make a change. pushb 3 to end the loop and print out previous version)
 
 class filesystem:
     def time(timestamp):
         curr_dt = datetime.now()
         timestamp = int(round(curr_dt.timestamp()))
-        version = {diff1: 'version1', diff2: 'version2', diff3: "version3"}
         return timestamp
     
 
@@ -18,23 +17,27 @@ class VersionFile:
         self.versions = OrderedMap()
         file_string = self.read_from_file()
         initial_diff = StringDiff("", file_string)
-        timestamp = int(round(curr_dt.timestamp()))
         curr_dt = datetime.now()
+        timestamp = int(round(curr_dt.timestamp()))
         self.versions[timestamp] = initial_diff
-        return timestamp
     
     def save(self):
         previous_version = self.build_latest()
         current_version = self.read_from_file()
-
+        updated = StringDiff(previous_version, current_version)
+        curr_dt = datetime.now()
+        timestamp = int(round(curr_dt.timestamp()))
+        self.versions[timestamp] = updated
+        
     def read_from_file(self):
         with open(self.file_path, 'r') as file:
-            content = file.read()
+            content = file.read()  
         return (content)
+    
     def build_latest(self):
         current = ""
         for _, diff in self.versions:
-            StringDiff.apply_diff(current, diff)
+            current = StringDiff.apply_diff(current, diff)
 
         return current
         
